e5e4333edd8aff081db8520a1ac7b1669c5e59b1
Morten Søgård Jensen <morten.sogard.jensen@systematic.com>
2022-12-28 10:46:34 +0100

    Day 1

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,32 +1,67 @@
+use std::collections::VecDeque;
+
 pub fn main() {
-    println!("Day");
+    println!("Day 1");
 }
 
-type Input = String;
-
+type Input = Vec<isize>;
 
 fn puzzle_1(input: Input) -> usize {
-    0
+    let mut last_number = None;
+
+    input.iter().fold(0, |acc, v| {
+        let mut ret = acc;
+
+        if let Some(prev) = last_number {
+            if prev < v {
+                ret += 1;
+            }
+        }
+        last_number = Some(v);
+
+        ret
+    })
 }
 
 fn puzzle_2(input: Input) -> usize {
-    0
+    let mut window: VecDeque<isize> = VecDeque::new();
+
+    input.iter().fold(0, |acc, v| {
+        let mut ret = acc;
+        if window.len() == 3 {
+            let prev_sum = window.iter().sum::<isize>();
+            window.pop_front();
+            window.push_back(*v);
+            let new_sum = window.iter().sum::<isize>();
+            if new_sum > prev_sum {
+                ret += 1;
+            }
+        } else {
+            window.push_back(*v);
+
+        }
+
+        
+        ret
+    })
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::util;
+
     use super::*;
 
     #[test]
     fn it_works() {
-        assert_eq!(1, puzzle_1(parse(include_str!("puzzle_1_test.txt"))));
-        assert_eq!(1, puzzle_1(parse(include_str!("puzzle_1.txt"))));
+        assert_eq!(7, puzzle_1(parse(include_str!("puzzle_1_test.txt"))));
+        assert_eq!(1655, puzzle_1(parse(include_str!("puzzle_1.txt"))));
 
-        assert_eq!(1, puzzle_2(parse(include_str!("puzzle_1_test.txt"))));
-        assert_eq!(1, puzzle_2(parse(include_str!("puzzle_1.txt"))));
+        assert_eq!(5, puzzle_2(parse(include_str!("puzzle_1_test.txt"))));
+        assert_eq!(1683, puzzle_2(parse(include_str!("puzzle_1.txt"))));
     }
 
     fn parse(s: &str) -> Input {
-        s.to_string()
+        s.lines().map(|s| util::parse_numbers(s)[0]).collect()
     }
 }
