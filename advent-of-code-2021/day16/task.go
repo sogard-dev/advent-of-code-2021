87f3c536d03525e273180626a2cd08bea8efb1a4
morten <morten.sogard.jensen@gmail.com>
2024-11-15 10:05:46 +0000

    day 16

--- advent-of-code-2021/day16/task.go
+++ advent-of-code-2021/day16/task.go
@@ -2,19 +2,202 @@ package day16
 
 import (
 	"fmt"
-	"strings"
+	"math"
+	"strconv"
 )
 
-func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+func binToInt(str string) int {
+	i, _ := strconv.ParseInt(str, 2, 64)
+	return int(i)
+}
+
+func strToBin(str string) string {
+	ret := []rune{}
+	for _, s := range str {
+		num, _ := strconv.ParseInt(string(s), 16, 16)
+
+		b := fmt.Sprintf("%04b", num)
+		for _, c := range b {
+			ret = append(ret, c)
+		}
 	}
-	return 0
+	return string(ret)
+}
+
+type literal struct {
+	packetVersion int
+	packetType    int
+	literal       int
+}
+
+type operator struct {
+	packetVersion int
+	packetType    int
+	packets       []packet
+}
+
+func (l literal) getPacketVersion() int {
+	return l.packetVersion
+}
+
+func (l literal) value() int {
+	return l.literal
+}
+
+func (o operator) getPacketVersion() int {
+	return o.packetVersion
+}
+
+func (o operator) value() int {
+	switch o.packetType {
+	case 0:
+		s := 0
+		for _, c := range o.packets {
+			s += c.value()
+		}
+		return s
+	case 1:
+		s := 1
+		for _, c := range o.packets {
+			s *= c.value()
+		}
+		return s
+	case 2:
+		s := math.MaxInt
+		for _, c := range o.packets {
+			s = min(s, c.value())
+		}
+		return s
+	case 3:
+		s := -1
+		for _, c := range o.packets {
+			s = max(s, c.value())
+		}
+		return s
+	case 5:
+		if o.packets[0].value() > o.packets[1].value() {
+			return 1
+		}
+		return 0
+	case 6:
+		if o.packets[0].value() < o.packets[1].value() {
+			return 1
+		}
+		return 0
+	case 7:
+		if o.packets[0].value() == o.packets[1].value() {
+			return 1
+		}
+		return 0
+	}
+
+	panic("Unknown package")
+}
+
+type packet interface {
+	getPacketVersion() int
+	value() int
+}
+
+func readLiteral(msg string, ptr int) (int, literal) {
+	v := binToInt(msg[ptr : ptr+3])
+	t := binToInt(msg[ptr+3 : ptr+6])
+
+	ptr = ptr + 6
+	b := ""
+	for {
+		b = b + msg[ptr+1:ptr+5]
+		ptr = ptr + 5
+		if msg[ptr-5] == '0' {
+			break
+		}
+	}
+
+	return ptr, literal{
+		packetVersion: v,
+		packetType:    t,
+		literal:       binToInt(b),
+	}
+}
+
+func readOperator(msg string, ptr int) (int, operator) {
+	v := binToInt(msg[ptr : ptr+3])
+	t := binToInt(msg[ptr+3 : ptr+6])
+	i := binToInt(msg[ptr+6 : ptr+7])
+	ptr = ptr + 7
+
+	op := operator{
+		packetVersion: v,
+		packetType:    t,
+		packets:       []packet{},
+	}
+
+	if i == 0 {
+		toRead := binToInt(msg[ptr : ptr+15])
+		ptr = ptr + 15
+		stopWhen := toRead + ptr
+		for ptr < stopWhen {
+			ptr = readPacket(msg, &op, ptr)
+		}
+	} else {
+		toRead := binToInt(msg[ptr : ptr+11])
+		ptr = ptr + 11
+		for len(op.packets) < toRead {
+			ptr = readPacket(msg, &op, ptr)
+		}
+	}
+
+	return ptr, op
+}
+
+func readPacket(msg string, parent *operator, ptr int) int {
+	t := binToInt(msg[ptr+3 : ptr+6])
+
+	if t == 4 {
+		p, l := readLiteral(msg, ptr)
+		parent.packets = append(parent.packets, l)
+		return p
+	} else {
+		p, l := readOperator(msg, ptr)
+		parent.packets = append(parent.packets, l)
+		return p
+	}
+}
+
+func readPackets(msg string) packet {
+	rootPacket := operator{
+		packets: []packet{},
+	}
+
+	ptr := 0
+	for ptr+10 < len(msg) {
+		ptr = readPacket(msg, &rootPacket, ptr)
+	}
+
+	return rootPacket
+}
+
+func sumPacketVersions(p packet) int {
+	s := p.getPacketVersion()
+
+	switch v := p.(type) {
+	case operator:
+		for _, c := range v.packets {
+			s += sumPacketVersions(c)
+		}
+	}
+
+	return s
+}
+
+func part1(input string) int {
+	bin := strToBin(input)
+	packets := readPackets(bin)
+	return sumPacketVersions(packets)
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
-	}
-	return 0
+	bin := strToBin(input)
+	packets := readPackets(bin)
+	return packets.value()
 }
--- advent-of-code-2021/day16/task_test.go
+++ advent-of-code-2021/day16/task_test.go
@@ -8,11 +8,21 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 31, part1(`A0016C880162017C3686B18A3D4780`))
+	require.Equal(t, 23, part1(`C0015000016115A2E0802F182340`))
+	require.Equal(t, 12, part1(`620080001611562C8802118E34`))
+	require.Equal(t, 16, part1(`8A004A801A8002F478`))
+	require.Equal(t, 960, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 3, part2(`C200B40A82`))
+	require.Equal(t, 54, part2(`04005AC33890`))
+	require.Equal(t, 7, part2(`880086C3E88112`))
+	require.Equal(t, 9, part2(`CE00C43D881120`))
+	require.Equal(t, 1, part2(`D8005AC2A8F0`))
+	require.Equal(t, 0, part2(`F600BC2D8F`))
+	require.Equal(t, 0, part2(`9C005AC2F8F0`))
+	require.Equal(t, 1, part2(`9C0141080250320F1802104A08`))
+	require.Equal(t, 12301926782560, part2(utils.GetInput(t, "input.txt")))
 }
