be5d0d2c5a26e5139f9c77a3faab092a4ea4098c
morten <morten.sogard.jensen@gmail.com>
2024-11-21 08:54:16 +0000

    day 20

--- advent-of-code-2021/day20/task.go
+++ advent-of-code-2021/day20/task.go
@@ -1,20 +1,107 @@
 package day20
 
 import (
-	"fmt"
+	"math"
 	"strings"
 )
 
-func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+type pos struct {
+	x, y int
+}
+
+type minMax struct {
+	minX, maxX, minY, maxY int
+}
+
+func getMinMax(img map[pos]bool) minMax {
+	minX, maxX, minY, maxY := math.MaxInt, 0, math.MaxInt, 0
+	for p := range img {
+		minX = min(minX, p.x)
+		maxX = max(maxX, p.x)
+		minY = min(minY, p.y)
+		maxY = max(maxY, p.y)
 	}
-	return 0
+	return minMax{
+		minX: minX,
+		maxX: maxX,
+		minY: minY,
+		maxY: maxY,
+	}
+}
+
+func iterate(img map[pos]bool, algo *string, run int, size minMax) (map[pos]bool, minMax) {
+	newMinMax := minMax{
+		minX: size.minX - 1,
+		maxX: size.maxX + 1,
+		minY: size.minY - 1,
+		maxY: size.maxY + 1,
+	}
+
+	isEdge := func(p pos) bool {
+		if (*algo)[0] == '#' && run%2 == 1 {
+			if p.x <= newMinMax.minX || p.x >= newMinMax.maxX || p.y <= newMinMax.minY || p.y >= newMinMax.maxY {
+				return true
+			}
+		}
+		return false
+	}
+
+	newImage := map[pos]bool{}
+	for y := newMinMax.minY; y <= newMinMax.maxY; y++ {
+		for x := newMinMax.minX; x <= newMinMax.maxX; x++ {
+			p := pos{
+				x: x,
+				y: y,
+			}
+
+			v := 256
+			a := 0
+			for dy := -1; dy <= 1; dy++ {
+				for dx := -1; dx <= 1; dx++ {
+					np := pos{x: p.x + dx, y: p.y + dy}
+					if img[np] || isEdge(np) {
+						a += v
+					}
+
+					v /= 2
+				}
+			}
+
+			if (*algo)[a] == '#' {
+				newImage[p] = true
+			}
+		}
+	}
+
+	return newImage, newMinMax
+}
+
+func part1(input string) int {
+	return solve(input, 2)
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	return solve(input, 50)
+}
+
+func solve(input string, runs int) int {
+	blocks := strings.Split(input, "\n\n")
+	algo := blocks[0]
+
+	image := map[pos]bool{}
+
+	for y, line := range strings.Split(blocks[1], "\n") {
+		for x, r := range line {
+			if r == '#' {
+				image[pos{x: x, y: y}] = true
+			}
+		}
 	}
-	return 0
+
+	size := getMinMax(image)
+	for run := range runs {
+		image, size = iterate(image, &algo, run, size)
+	}
+
+	return len(image)
 }
--- advent-of-code-2021/day20/task_test.go
+++ advent-of-code-2021/day20/task_test.go
@@ -8,11 +8,19 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 35, part1(testInput))
+	require.Equal(t, 5437, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 3351, part2(testInput))
+	require.Equal(t, 19340, part2(utils.GetInput(t, "input.txt")))
 }
+
+const testInput = `..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###`
